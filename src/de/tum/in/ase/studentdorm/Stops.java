33048ab766654b33f349f9a211dced4f48b109b4
ge32goy <hanyin.chen@tum.de>
2022-12-16 11:07:21 +0100

    commit

--- src/de/tum/in/ase/studentdorm/Stops.java
+++ src/de/tum/in/ase/studentdorm/Stops.java
@@ -29,9 +29,7 @@ public class Stops {
      * This method takes a direction value and based on that value it checks if the corresponding list is empty.
      */
     public boolean isEmpty(Direction direction) throws IllegalArgumentException {
-        if (direction == Direction.IDLE) {
-            throw new IllegalArgumentException("Direction cannot be IDLE");
-        } else if (direction == Direction.UP) {
+        if (direction == Direction.UP) {
             return stopsUp.isEmpty();
         } else {
             return stopsDown.isEmpty();
