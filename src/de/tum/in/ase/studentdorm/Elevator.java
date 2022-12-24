ed2fb9f3bab05e8e086fec085fe95207a3ff4fe1
ge32goy <hanyin.chen@tum.de>
2022-12-24 12:37:03 +0100

    commit

--- src/de/tum/in/ase/studentdorm/Elevator.java
+++ src/de/tum/in/ase/studentdorm/Elevator.java
@@ -98,13 +98,13 @@ public class Elevator {
             }
         } else {
             if (!this.stops.isEmpty(this.direction)) {
-                if (this.direction == Direction.UP && this.currentFloor != this.maxFloor && this.stops.getStopsUp().contains(this.currentFloor)) {
+                if (this.direction == Direction.UP && this.stops.getStopsUp().contains(this.currentFloor)) {
                     this.currentFloor++;
                     if (this.currentFloor == this.stops.getStopsUp().get(0)) {
                         this.stops.remove(this.direction, 0);
                         return true;
                     }
-                } else if (this.direction == Direction.DOWN && this.currentFloor != 0 && this.stops.getStopsDown().contains(this.currentFloor)) {
+                } else if (this.direction == Direction.DOWN && this.stops.getStopsDown().contains(this.currentFloor)) {
                     this.currentFloor--;
                     if (this.currentFloor == this.stops.getStopsDown().get(0)) {
                         this.stops.remove(this.direction, 0);
