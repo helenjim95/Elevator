3233045459df7a23df7f5b0e6435d6ccf6fd79ce
ge32goy <hanyin.chen@tum.de>
2022-12-25 18:38:20 +0100

    commit

--- src/de/tum/in/ase/studentdorm/Elevator.java
+++ src/de/tum/in/ase/studentdorm/Elevator.java
@@ -94,12 +94,9 @@ public class Elevator {
         if (this.direction == Direction.IDLE) {
             if (!this.stops.isEmpty(Direction.UP)) {
                 this.direction = Direction.UP;
-                this.currentFloor++;
             } else if (!this.stops.isEmpty(Direction.DOWN)) {
                 this.direction = Direction.DOWN;
-                this.currentFloor--;
             }
-            return false;
         } else {
             if (!this.stops.isEmpty(this.direction)) {
                 if (this.direction == Direction.UP && this.currentFloor != this.maxFloor) {
