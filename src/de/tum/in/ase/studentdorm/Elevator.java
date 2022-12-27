b245cc720d2d6da2925ee98ec494bc6ff8b9e189
ge32goy <hanyin.chen@tum.de>
2022-12-27 17:15:52 +0100

    commit

--- src/de/tum/in/ase/studentdorm/Elevator.java
+++ src/de/tum/in/ase/studentdorm/Elevator.java
@@ -126,43 +126,43 @@ public class Elevator {
 //        Be sure to sort the passenger's list in the method openDoor based on the order
 //        already introduced. Of course, the person who sadly has to be kicked out
 //        should not be in the list.
-        if (this.passengers.size() < this.capacity) {
-            this.passengers.add(person);
-            if (person.getDestinationFloor() > this.currentFloor) {
-                this.stops.addStop(Direction.UP, person.getDestinationFloor());
-            } else if (person.getDestinationFloor() < this.currentFloor) {
-                this.stops.addStop(Direction.DOWN, person.getDestinationFloor());
-            }
-            return true;
-        } else {
-            Comparator<Person> comparator = (p1, p2) -> {
-                int p1Distance = p1.computeDistance(this.currentFloor);
-                int p2Distance = p2.computeDistance(this.currentFloor);
-                if (p1Distance == p2Distance) {
-                    if (Direction.computeDirection(this.currentFloor, p1.getDestinationFloor()) == Direction.DOWN) {
-                        return 1;
-                    } else {
-                        return -1;
-                    }
+        if (person != null) {
+            if (this.passengers.size() < this.capacity) {
+                this.passengers.add(person);
+                if (person.getDestinationFloor() > this.currentFloor) {
+                    this.stops.addStop(Direction.UP, person.getDestinationFloor());
+                } else if (person.getDestinationFloor() < this.currentFloor) {
+                    this.stops.addStop(Direction.DOWN, person.getDestinationFloor());
                 }
-                return p1Distance - p2Distance;
-            };
-            this.passengers.sort(comparator);
-            ListIterator<Person> iterator = this.passengers.listIterator();
-            while (iterator.hasNext()) {
-                Person p = iterator.next();
-                if (p.getDestinationFloor() == this.currentFloor) {
-                    iterator.remove();
-                    if (this.direction != Direction.IDLE) {
-                    this.stops.remove(this.direction, p.getDestinationFloor());
+                return true;
+            } else {
+                Comparator<Person> comparator = (p1, p2) -> {
+                    int p1Distance = p1.computeDistance(this.currentFloor);
+                    int p2Distance = p2.computeDistance(this.currentFloor);
+                    if (p1Distance == p2Distance) {
+                        if (Direction.computeDirection(this.currentFloor, p1.getDestinationFloor()) == Direction.DOWN) {
+                            return 1;
+                        } else {
+                            return -1;
+                        }
                     }
-                }
-                if (p.computeDistance(this.currentFloor) < person.computeDistance(this.currentFloor)) {
-                    iterator.remove();
-                    this.passengers.add(person);
-                    return true;
-                } else if (p.computeDistance(this.currentFloor) == person.computeDistance(this.currentFloor)) {
-                    if (Direction.computeDirection(this.currentFloor, p.getDestinationFloor()) == Direction.DOWN) {
+                    return p1Distance - p2Distance;
+                };
+                this.passengers.sort(comparator);
+                ListIterator<Person> iterator = this.passengers.listIterator();
+                while (iterator.hasNext()) {
+                    Person p = iterator.next();
+                    if (p.getDestinationFloor() == this.currentFloor) {
+                        iterator.remove();
+                        if (this.direction != Direction.IDLE) {
+                            this.stops.remove(this.direction, p.getDestinationFloor());
+                        }
+                    }
+                    if (p.computeDistance(this.currentFloor) < person.computeDistance(this.currentFloor)) {
+                        iterator.remove();
+                        this.passengers.add(person);
+                        return true;
+                    } else if (p.computeDistance(this.currentFloor) == person.computeDistance(this.currentFloor) && Direction.computeDirection(this.currentFloor, p.getDestinationFloor()) == Direction.DOWN) {
                         iterator.remove();
                         this.passengers.add(person);
                         return true;
